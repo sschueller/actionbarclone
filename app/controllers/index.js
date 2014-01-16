831435ee9c18a6bc6250a79d6efffb7f175b7425
Stefan Schueller <sschueller@techdroid.com>
2014-01-16 09:42:54 +0100

    Added Hamburger

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,85 +1,183 @@
-// setTheme will automatically set:
-// setBlackAngle or setWhiteAngle
-// setBackground
-// setTitleColor 
-// 
-//$.headerbar.setTheme('black'); 
-// * 
-// Start Configuration
-$.headerbar.setParentContainer($.index);			// a reference to the window containing the widget
-$.headerbar.setBackground({							// background color of the bar
-	color:'#cacaca'
-});
-//$.headerbar.setAppIcon('/appiconcopy.png'); 		// if no icon is provided, the apps default one will be used
-$.headerbar.setBlackAngle();						// color of the angle
-$.headerbar.setTitle({								// the title of the containing window
-	text:'Window Title',
-	color:'#6f6f6f'
-});
-$.headerbar.setBack(function(){						// if angle is shown, what will happen uppon click.  If don't want the button to do anything, send empty function
-	alert('Clicked home');
+var backAction=null;
+var burgerAction=null;
+var extraButtonAction=null;
+var parentWindow=null;
+
+var buttonFactory=require(WPATH('button'));
+
+// set default app icon, in case no icon is provided
+$.appicon.backgroundImage='/appicon.png';	
+
+function openInflater(evt){
+	// need to code this part
+	alert('Open the Inflater');
+}
+
+$.headerbar.addEventListener('click',function(evt){
+	// this cancels the click event that get's fired 
+	// when the parent window is clicked 
+	evt.cancelBubble=true;
 });
 
-$.headerbar.hideBottomLine();						// for Holo Dark
-$.headerbar.showAngle();							// show or hide
-$.headerbar.setActionButtons(getExtraButtons());		// action buttons, inflater buttons and menu buttons
-// End Configuration
+$.backbutton.addEventListener('click',function(evt){
+	evt.cancelBubble=true;
+});
 
+$.backbutton.addEventListener('touchstart',function(evt){	
+	this.backgroundColor="#000";
+	this.opacity=0.2;
+});
 
-// For clarity, I'm using a function to get the Buttons Object
-// In a cross-platform scenario you'd want to have your menu options returned
-// by a function so you can then decide where and how display them
-function getExtraButtons(){
-	return {
-		visible:[
-			{
-				icon:'/ic_menu_copy_holo_light.png',
-				title: 'Copy',
-				action: function (){
-					alert('Copy');
-				}
-			},
-			{
-				icon:'/ic_menu_share_holo_light.png',
-				title: 'Share',
-				action: function (){
-					alert('Share');
-				}
-			}],
-		inflater:[
-			{
-				title: 'Some Action #1',
-				action: function (){
-					alert('action1');
-				}
-			},
-			{
-				title: 'Some Action #2',
-				action: function (){
-					alert('action2');
-				}
-			}],
-		androidmenu:[
-			{
-				title: 'Settings',
-				action: function (){
-					alert('Settings');
-				}
-			},
-			{
-				title: 'Help',
-				action: function (){
-					alert('Help');
-				}
-			},
-			{
-				title: 'About',
-				action: function (){
-					alert('About');
-				}
-			}
-		]
+$.backbutton.addEventListener('touchend',function(evt){
+	this.backgroundColor='transparent';
+	this.opacity=1;
+	(!backAction)?parentWindow.close():backAction();
+});
+
+function setTitle(args){
+	$.headertitle.text=args.text;
+	$.headertitle.color=args.color;
+}
+
+function setBack(action){
+	backAction=action;
+}
+
+function setBurger(action){
+	burgerAction=action;
+}
+
+function setBlackAngle(){
+	$.backangle.backgroundImage=WPATH('light-back-angle.png');
+}
+
+function setWhiteAngle(){
+	$.backangle.backgroundImage=WPATH('white-back-angle.png');
+}
+
+function setBlackBurger(){
+	$.burger.backgroundImage=WPATH('ic_drawer_holo_dark.png');
+}
+
+function setWhiteBurger(){
+	$.burger.backgroundImage=WPATH('ic_drawer_holo_light.png');
+}
+
+function hideAngle(){
+	//$.backangle.width='0';
+	$.backangle.visible=false;
+}
+
+function showAngle(){
+	//$.backangle.width='14dp';
+	$.backangle.visible=true;
+}
+
+function hideBurger(){
+	$.burger.visible=false;
+}
+
+function showBurger(){
+	$.burger.visible=true;
+}
+
+function setBurgerWidth(offset){
+	var min = 13;
+	var max = 20;
+
+	var newWidth = max - ((max - min) * offset).toFixed();
+	
+	$.burger.width= newWidth;
+}
+
+function setAppIcon(icon){
+	$.appicon.backgroundImage=icon;	
+}
+
+function setParentContainer(handle){
+	parentWindow=handle;
+}
+
+function setBackground(args){
+//	(args.image)?$.headerbar.backgroundImage=args.image:$.headerbar.backgroundImage=WPATH('lightbg.png');
+	$.headerbar.backgroundColor=args.color;
+	
+	$.headerbar.backgroundRepeat= true;
+}
+
+function showBottomLine(){
+	$.bottomline.visible=true;
+}
+
+function hideBottomLine(){
+	$.bottomline.visible=false;
+}
+
+function setTop(top){
+	this.headerbar.top=top;
+}
+
+function setActionButtons(args){
+	if (args.visible){
+		args.visible.forEach(function(button){
+			var payload={
+				image:button.icon,
+				action: button.action,
+				title: button.title
+			};
+			var tbbutton=buttonFactory.getButton(payload);
+			$.actionButtons.add(tbbutton);
+		});
+	}
+
+	if (args.inflater){
+		var menuoptions='';
+		var payload={
+			image: WPATH('ic_menu_moreoverflow_normal_holo_light.png')
+		};
+		
+		var inflater=buttonFactory.getButton(payload);
+		args.inflater.forEach(function(button){
+			// here I gather all the menu options and build a dropdown menu
+			menuoptions+=button.title;
+		});
+		inflater.addEventListener('click',openInflater);
+			
+		$.actionButtons.add(inflater);
+	}
+
+	if (args.androidmenu){
+		parentWindow.activity.onCreateOptionsMenu = function(e){
+			var menu = e.menu;
+			args.androidmenu.forEach(function(button){
+				var menuItem = menu.add({ 
+					title: button.title,
+					icon: '/ic_menu_copy_holo_light.png',
+					showAsAction : Ti.Android.SHOW_AS_ACTION_NEVER
+				});
+				menuItem.addEventListener("click", button.action);
+			});
+		};
 	}
 }
 
-$.index.open();
+//
+exports.setTop=setTop;
+exports.setActionButtons=setActionButtons;
+exports.showBottomLine=showBottomLine;
+exports.hideBottomLine=hideBottomLine;
+exports.setBackground=setBackground;
+exports.setParentContainer=setParentContainer;
+exports.setAppIcon=setAppIcon;
+exports.hideAngle=hideAngle;
+exports.showAngle=showAngle;
+exports.setBlackAngle=setBlackAngle;
+exports.setWhiteAngle=setWhiteAngle;
+exports.hideBurger=hideBurger;
+exports.showBurger=showBurger;
+exports.setBlackBurger=setBlackBurger;
+exports.setWhiteBurger=setWhiteBurger;
+exports.setTitle=setTitle;
+exports.setBack=setBack;
+exports.setBurger=setBurger;
+exports.setBurgerWidth=setBurgerWidth;
